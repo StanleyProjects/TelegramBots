45b7fab5759a681dc8fbe1908dd498f3ca780609
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-06 18:27:38 +0300

    tests

--- src/test/bash/get_me_test.sh
+++ src/test/bash/get_me_test.sh
@@ -10,11 +10,11 @@ ACTUAL_VALUE="$(${ISSUER} 0 0)"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Wrong arguments!'
 
-ACTUAL_VALUE="$(${ISSUER} '')"
+ACTUAL_VALUE="$(TG_BOT_ID='' TG_BOT_TOKEN='' ${ISSUER} '')"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_ID" is empty!'
 
-ACTUAL_VALUE="$(TG_BOT_ID=0 ${ISSUER} '')"
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN='' ${ISSUER} '')"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_TOKEN" is empty!'
 
--- src/test/bash/send_document_test.sh
+++ src/test/bash/send_document_test.sh
@@ -14,15 +14,15 @@ ACTUAL_VALUE="$(${ISSUER} 0 0 0)"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Wrong arguments!'
 
-ACTUAL_VALUE="$(${ISSUER} '' '')"
+ACTUAL_VALUE="$(TG_BOT_ID='' TG_BOT_TOKEN='' TG_CHAT_ID='' ${ISSUER} '' '')"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_ID" is empty!'
 
-ACTUAL_VALUE="$(TG_BOT_ID=0 ${ISSUER} '' '')"
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN='' TG_CHAT_ID='' ${ISSUER} '' '')"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_BOT_TOKEN" is empty!'
 
-ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} '' '')"
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 TG_CHAT_ID='' ${ISSUER} '' '')"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_CHAT_ID" is empty!'
 
