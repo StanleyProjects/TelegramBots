ed41ecd12b9e206b8d48c3c355a2ad405bff8bbb
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-06 18:15:17 +0300

    tests

--- src/test/bash/unit_test.sh
+++ src/test/bash/unit_test.sh
@@ -2,8 +2,10 @@
 
 tests='src/test/bash'
 asserts="${tests}/asserts"
+tgbots="src/main/bash"
 
 . $tests/readme_test.sh
 . $tests/license_test.sh
+. $tests/get_me_test.sh
 
 echo 'All tests were successful.'
