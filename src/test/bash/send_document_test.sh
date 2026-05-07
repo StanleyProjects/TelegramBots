7332d098b83bec37cc5a7160e9017a89a74b67aa
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-07 19:36:06 +0300

    tests

--- src/test/bash/send_document_test.sh
+++ src/test/bash/send_document_test.sh
@@ -37,3 +37,22 @@ ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} 0 '' '')"
 ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} 0 0 '')"
 . $asserts/ne.sh $? 0
 . $asserts/eq.sh "${ACTUAL_VALUE}" 'Argument "TG_INPUT" is empty!'
+
+POINTER="$(TZ=utc LC_ALL=C date +%Y%m%d%H%M%S%3N)"
+TEST_INPUT="/tmp/${POINTER}.txt"
+
+rm "${TEST_INPUT}" 2> /dev/null
+[[ ! -f "${TEST_INPUT}" ]] || exit 1
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} 0 0 "${TEST_INPUT}")"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" "No file \"${TEST_INPUT}\"!"
+
+echo -n '' > "${TEST_INPUT}"
+[[ -f "${TEST_INPUT}" && ! -s "${TEST_INPUT}" ]] || exit 1
+
+ACTUAL_VALUE="$(TG_BOT_ID=0 TG_BOT_TOKEN=0 ${ISSUER} 0 0 "${TEST_INPUT}")"
+. $asserts/ne.sh $? 0
+. $asserts/eq.sh "${ACTUAL_VALUE}" "File \"${TEST_INPUT}\" is empty!"
+
+rm "${TEST_INPUT}"
